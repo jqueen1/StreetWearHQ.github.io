7ec828a93adfbabc2e42bd250d85dc4d17117c98
Jessica <j.queen_xx@yahoo.com>
2020-08-30 05:40:12 +0000

    alot

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// function nextPage(){
+// document.getElementById("test").addEventListener("click", displayDate);
+// }
+
+
+
+
+document.getElementById("test").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("You would now be directed to the womens page");
+}
